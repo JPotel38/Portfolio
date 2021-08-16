45f3aed8469a08d1ef00ce578184851c3f6dce26
Jérémy Potel <potel.jeremy@gmail.com>
2021-08-16 23:12:40 +0200

    WIP design nav

--- src/screens/Nav.js
+++ src/screens/Nav.js
@@ -1,15 +1,6 @@
 import React from 'react'
 import {Layout, Menu} from 'antd';
-import {
-    BookOutlined,
-    DesktopOutlined,
-    EuroCircleOutlined,
-    GithubFilled,
-    HomeOutlined,
-    LinkedinFilled,
-    RocketOutlined,
-    ThunderboltOutlined
-} from '@ant-design/icons';
+import {HomeOutlined, GithubFilled, LinkedinFilled} from '@ant-design/icons';
 import {Link} from 'react-router-dom'
 
 const {Sider} = Layout;
@@ -20,33 +11,28 @@ export default function Nav() {
         <Sider
             breakpoint="md"
             collapsedWidth={0}>
-            <Menu theme="dark" mode="inline" style={{paddingTop: 60, width: '100%'}}>
+            <Menu mode="inline" style={{paddingTop: 60, width: '100%'}}>
                 <Menu.Item key="0" style={{height: 50, marginBottom: 50}} icon={<HomeOutlined style={{fontSize: 50}}/>}>
                     <Link to='/Portfolio'/>
                 </Menu.Item>
-                <Menu.Item key="1" icon={<BookOutlined style={{fontSize: 25}}/>}>
-                    <Link to='/education'>Education</Link>
+                <Menu.Item key="1"><Link to='/education'>Education</Link>
                 </Menu.Item>
-                <Menu.Item key="2" icon={<DesktopOutlined style={{fontSize: 25}}/>}>
-                    <Link to='/projets'>Projets</Link>
+                <Menu.Item key="2"><Link to='/projets'>Projets</Link>
                 </Menu.Item>
-                <Menu.Item key="3" icon={<EuroCircleOutlined style={{fontSize: 25}}/>}>
-                    <Link to='/pro'>Emplois</Link>
+                <Menu.Item key="3"><Link to='/pro'>Emplois</Link>
                 </Menu.Item>
-                <Menu.Item key="4" icon={<RocketOutlined style={{fontSize: 25}}/>}>
-                    <Link to='/perso'>Projets personnels</Link>
+                <Menu.Item key="4"><Link to='/perso'>Projets personnels</Link>
                 </Menu.Item>
-                <Menu.Item key="5" icon={<ThunderboltOutlined style={{fontSize: 25}}/>}
-                           style={{height: 50, fontSize: 15}}>
-                    <Link to='/game'>Game Zone</Link>
+                <Menu.Item key="5"><Link to='/game'>Game Zone</Link>
                 </Menu.Item>
                 <Menu.Item key="6">
-                    <a href="https://github.com/JPotel38" target="_blank" rel="noopener noreferrer">
-                        <GithubFilled style={{color: '#92D050', fontSize: 25, marginTop: 10, marginBottom: 10}}/>Github</a>
+                    <GithubFilled id="github-icon"/>
+                    <a href="https://github.com/JPotel38" target="_blank" rel="noopener noreferrer">Github</a>
                 </Menu.Item>
                 <Menu.Item key="7">
+                    <LinkedinFilled id="linkedin-icon"/>
                     <a href="https://www.linkedin.com/in/jérémy-potel/" target="_blank" rel="noopener noreferrer">
-                        <LinkedinFilled style={{color: 'blue', fontSize: 25, marginTop: 10,}}/>LinkedIn</a>
+                        LinkedIn</a>
                 </Menu.Item>
             </Menu>
         </Sider>
