e3e460362dcd25b8bc880cf7a4ff8e20c0d28258
Jérémy Potel <potel.jeremy@gmail.com>
2023-09-04 10:06:02 +0200

    style: navbar
    refacto: routing

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,32 +1,30 @@
 import React from 'react'
-import {HashLink} from "react-router-hash-link";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faLinkedin} from "@fortawesome/free-brands-svg-icons/faLinkedin";
-import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import {faLinkedin, faGithub, faInstagram} from "@fortawesome/free-brands-svg-icons";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import './Nav.scss';
 
 export default function Nav() {
     return (
-        <Navbar expand="lg" >
+        <Navbar expand="lg">
             <Container>
-                <Navbar.Brand key="0"><HashLink smooth to='/'>Home</HashLink>
+                <Navbar.Brand href={'/'} className={'current-navbar-brand'}>Home</Navbar.Brand>
+                <Navbar.Brand href={'portfolio'}>Portfolio
                 </Navbar.Brand>
-                <Navbar.Brand key="2"><HashLink smooth to='/portfolio'>Portfolio</HashLink>
+                <Navbar.Brand href={'experience'}>Experience
                 </Navbar.Brand>
-                <Navbar.Brand key="3"><HashLink smooth to='/experience'>Experience</HashLink>
+                <Navbar.Brand href={'about'}>À propos </Navbar.Brand>
+                <Navbar.Brand href="https://github.com/JPotel38" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faGithub}/>
                 </Navbar.Brand>
-                <Navbar.Brand key="4"><HashLink smooth to='/about'>À propos</HashLink>
+                <Navbar.Brand href="https://www.linkedin.com/in/jérémy-potel/" target="_blank"
+                              rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faLinkedin}/>
                 </Navbar.Brand>
-                <Navbar.Brand key="5">
-                    <a href="https://github.com/JPotel38" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faGithub}/>
-                    </a>
-                </Navbar.Brand>
-                <Navbar.Brand key="6">
-                    <a href="https://www.linkedin.com/in/jérémy-potel/" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faLinkedin}/>
-                    </a>
+                <Navbar.Brand href="https://www.instagram.com/alvar.aotearoa/" target="_blank"
+                              rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faInstagram}/>
                 </Navbar.Brand>
             </Container>
         </Navbar>
